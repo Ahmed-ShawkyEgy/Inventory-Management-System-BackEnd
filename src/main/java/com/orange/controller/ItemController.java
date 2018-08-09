2d8d1ed9d41cceef0cc192f3729c571124391b83
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2018-08-09 10:01:39 +0200

    add Items controller

---
+++ src/main/java/com/orange/controller/ItemController.java
@@ -0,0 +1,79 @@
+package com.orange.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.orange.exception.ResourceNotFoundException;
+import com.orange.model.Item;
+import com.orange.repository.ItemRepository;
+
+
+@RestController
+@RequestMapping("/api")
+public class ItemController {
+	
+    @Autowired
+    ItemRepository itemRepository;
+    
+    @GetMapping("/items")
+    public List<Item> getAllItems()
+    {
+    	return itemRepository.findAll();
+    }
+    
+    // Create
+    @PostMapping("/items")
+    public Item registerItem(@Valid @RequestBody Item item)
+    {
+    	return itemRepository.save(item);
+    }
+    
+    // Read
+    @GetMapping("/items/{id}")
+    public Item getItemById(@PathVariable(value = "id") Long itemId) {
+        return itemRepository.findById(itemId)
+                .orElseThrow(() -> new ResourceNotFoundException("Item", "id", itemId));
+    }
+    
+    // Update
+    @PutMapping("/items/{id}")
+    public Item updateItem(@PathVariable(value = "id") Long itemId,
+            @Valid @RequestBody Item itemDetails) {
+
+		Item item = itemRepository.findById(itemId)
+		.orElseThrow(() -> new ResourceNotFoundException("Item", "id", itemId));
+
+		item.setName(itemDetails.getName());
+		item.setDescription(itemDetails.getDescription());
+		item.setPurchase_date(itemDetails.getPurchase_date());
+		
+		Item updatedItem = itemRepository.save(item);
+		return updatedItem;
+	}
+    
+    // Delete
+    @DeleteMapping("/items/{id}")
+    public ResponseEntity<?> deleteItem(@PathVariable(value = "id") Long itemId) {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new ResourceNotFoundException("Item", "id", itemId));
+
+        itemRepository.delete(item);
+
+        return ResponseEntity.ok().build();
+    }
+    
+    
+
+}
